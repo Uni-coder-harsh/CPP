150ad1464a7da4c577ac1c1e07cc3b4b898de283
Uni-coder-harsh <harsh06264@gmail.com>
2024-08-23 17:10:52 +0530

    Update T1.cpp

--- V1/T1.cpp
+++ V1/T1.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+/* Basic Program */
+#include <iostream>/*here iostream is header file which is use to give input and output*/
 
-int main(){
-    std::cout<<"Holla Ser";
-    return 0;
+int main(){/*int is integer*/ /*main is function and also the entry point   */
+    std::cout<<"Holla Ser";/*inside iostream there is std namespace under which is cout*/
+    return 0;/* Return 0 means successful termination of program*/
 }
