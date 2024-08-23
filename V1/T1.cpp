f874d33a0d57f1db00bb46ad92131a8b245c84f4
Uni-coder-harsh <harsh06264@gmail.com>
2024-08-23 19:53:58 +0530

    Updated after 3 lecture and learning Git

--- V1/T1.cpp
+++
@@ -1,7 +0,0 @@
-/* Basic Program */
-#include <iostream>/*here iostream is header file which is use to give input and output*/
-
-int main(){/*int is integer*/ /*main is function and also the entry point   */
-    std::cout<<"Holla Ser";/*inside iostream there is std namespace under which is cout*/
-    return 0;/* Return 0 means successful termination of program*/
-}
---
+++ V1/Vairiable.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    int sum= 45;
+    cout<<"Holla Ser "<< sum ;
+    return 0 ;
+}
