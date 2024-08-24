3762ceb84dcc3df9a7a0e74ae11d76585c8fc070
Uni-coder-harsh <harsh06264@gmail.com>
2024-08-24 19:16:03 +0530

    Input/Outpt Stream

---
+++ V5/V5.cpp
@@ -0,0 +1,17 @@
+# include<iostream>
+
+
+using namespace std;
+
+int main()
+{
+    int num1,num2;
+    cout<<"enter the value of num1: \n";/* << is a Insertion operator */
+    cin>>num1;/* >> is a Extraction operator */
+
+    cout<<"enter the value of num2: \n";/* << is a Insertion operator */
+    cin>>num2;/* >> is a Extraction operator */
+
+    cout<<"the sum is :"<< num1+num2;
+    return 0 ;
+}
