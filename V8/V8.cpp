b57247979a5634c905117b5f9c00a28ddb50e134
Uni-coder-harsh <harsh06264@gmail.com>
2024-08-26 11:17:01 +0530

    created V8

---
+++ V8/V8.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<iomanip>
+using namespace std ;
+
+int main(){
+
+    // ****Constant****
+    // const float pi =3.14;
+    // cout<< pi <<endl;
+   
+    // ****Manipulator****
+    // endl , setw are example of maniplulator
+    // int a=10,b=4444,c=76764755;
+    // cout<<setw(6)<<a<<endl;
+    // cout<<setw(6)<<b<<endl;
+    // cout<<setw(6)<<c<<endl;
+
+    //****Operator Precedence****
+    
+
+    return 0 ;
+
+}
