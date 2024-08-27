59b99fcf11fc5af8fc6c7ab084d8e0d9c5872f48
Uni-coder-harsh <harsh06264@gmail.com>
2024-08-27 16:51:18 +0530

    created V11

---
+++ V11/V11.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+    //  for (int i = 0; i < 40; i++)
+    //  {
+    //     cout<<i<<endl;    
+    //     if(i==5){
+    //     cout<<i<<endl;   
+    //         break;
+    //     }
+    //    cout<<i<<endl; 
+    //  }
+
+    for (int i = 0; i < 40; i++)
+    {
+        
+       if (i==5)
+       {
+        
+        continue;
+       }
+       cout<<i<<endl;
+    }
+    
+     
+ return 0;
+}
