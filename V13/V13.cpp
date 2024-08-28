6a9dd59ea464752798b340963dca0b6b22f7e41e
Uni-coder-harsh <harsh06264@gmail.com>
2024-08-28 22:16:43 +0530

    updated V13

---
+++ V13/V13.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+    //arrays example
+    int marks[]={28,85,63 ,46};
+    int maths[4];
+    maths[0]=74;
+    maths[1]=64;
+    maths[2]=73;
+    maths[3]=97;
+    cout<<marks[0]<<endl;
+    cout<<marks[1]<<endl;
+    cout<<marks[2]<<endl;
+    cout<<marks[3]<<endl;
+    cout<<maths[1]<<endl;
+
+    return 0;
+}
