13193a3e12fab827d1c53941d16dda6a8a797461
Uni-coder-harsh <harsh06264@gmail.com>
2024-09-03 19:14:59 +0530

    Updated the V15 cpp

--- V15/V15.cpp
+++ V15/V15.cpp
@@ -1,17 +1,25 @@
 #include<iostream>
-
 using namespace std;
 
-int sum(int a , int b ){
-    int c=a + b;
-    return c;
-}
+int sum(int a , int b);
+void g(void);
 int main(){
     int num1 , num2;
     cout<<"enter the num 1"<<endl;
     cin>>num1;
     cout<<"Enter the num 2 "<<endl;
     cin>>num2;
-    cout<<"the sum is :"<<num1+num2<<endl;
+    cout<<"the sum is :"<<sum(num1 , num2)<<endl;
+    g();
     return 0;
+    
+}
+
+int sum(int a , int b ){
+    int c=a + b;
+    return c;
+}
+
+void g(){
+    cout<<"hello";
 }
