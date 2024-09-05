6dc3d886edbd49b5126896acd1fb07dc583706be
Uni-coder-harsh <harsh06264@gmail.com>
2024-09-05 09:42:15 +0530

    updates the code

--- V16/V16.cpp
+++ V16/V16.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
-
 using namespace std;
 
-
+int sum(int a , int b){
+int c= a + b;
+return c;
+}
+// using call by refrence by pointers
+int swap(int* a , int* b){
+    int temp = *a;
+    *a=*b;
+    *b=temp;
+}
 int main(){
-     
- return 0;
+     int a=4 , b=5 ;
+     cout<<sum(a,b)<<endl;
+     swap(&a,&b);
+     cout<<a<<endl<<b;
+   return 0;
 }
