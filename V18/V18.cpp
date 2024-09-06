d461397ceed23e6bf5d99a4d7649fcb6cf557576
Uni-coder-harsh <harsh06264@gmail.com>
2024-09-06 16:37:32 +0530

    Update

---
+++ V18/V18.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+// Factorial
+// int factorial(n){
+//     if(n<=1){
+//         return 1;
+//         }
+//     return n* factorial(n-1);
+// }
+int fib(int n){
+    if(n<2){
+        return n;
+    }
+    return fib(n-2) + fib(n-1);
+}
+int main(){
+    // factorial of number
+    // 6!=6*5*4*3*2*1
+    //n!=n*(n-1)!
+    int a;
+    cout<<"enter the number";
+    cin>>a;
+    cout<<"fibonacchi at  "<< a <<" is "<<fib(a);
+    return 0;
+}
