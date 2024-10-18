dbdbb7deef62e946917b146aff07aa7ed23b94f4
uni-coder-harsh <harsh06264@gmail.com>
2024-10-18 20:35:23 +0530

    Update 1.1

---
+++ DSA/Lec 1 Things to Know in CPP/1.cpp
@@ -0,0 +1,115 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+//datatype
+
+    // long x=86;
+    // long long x=3464347735735773575;
+    // float x=959.8758;
+    // string s1 , s2 ;// string till break arives
+    // cin>>s1>>s2;
+    // cout<<s1<<s2;
+    // string str;
+    // getline(cin, str);//get fullline
+    // cout<<str;
+    // char a= 'f' ;
+    // cout<<a;
+    // return 0;
+
+//if-else-else if
+
+    // int age;
+    // cin>>age;
+    // if (age>=18)
+    // {
+    //     cout<<"adult";
+    // }
+    // else
+    // {
+    //     cout<<"not adult";
+    // }
+    
+//school grading system
+// int mark;
+// cin>>mark;
+//     if (mark<25)
+//     {
+//        cout<<"F";
+//     }
+//     else if (mark<44)
+//     {
+//         cout<<"E";
+//     }
+//     else if (mark<=49)
+//     {
+//         cout<<"D";
+//     }else if ( mark<=59)
+//     {
+//         cout<<"C";
+//     }else if ( mark<=79)
+//     {
+//         cout<<"B";
+//     }
+//     else if (mark<=100)
+//     {
+//         cout<<"A";
+//     }
+//     else
+//     {
+//         cout<<"wrong marks";
+//     }
+    
+
+// switch case
+    // int day;
+    // cin>>day;
+    // switch (day)
+    // {
+    // case 1:
+    //     cout<<"monday";
+    //     break;
+    // case 2:
+    //     cout<<"tuesday";
+    //     break;
+    // case 3:
+    //     cout<<"wednesday";
+    //     break;
+    // case 4:
+    //     cout<<"thursday";
+    //     break;
+    // case 5:
+    //     cout<<"friday";
+    //     break;
+    // case 6:
+    //     cout<<"saturday";
+    //     break;
+    // case 7:
+    //     cout<<"sunday";
+    //     break;
+    // default:
+    // cout<<"no such day";
+    //     break;
+    // }
+
+//Arrays
+    //arrays will be stored in cosecutive pattern but the first one is not been confirmed
+
+    //1D Array
+    // int arr[5];
+    // cin>>arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4];
+    // arr[3]+=175;
+    // cout<<arr[3];
+
+    //2D Array
+    // int a[3][5];
+    // a[1][3]=78;
+    // cout<<a[1][3];
+    // return 0;
+
+    //String as Arrays
+    // string s= "Harsh";
+    // int len=s.size(); //leangth of string
+    // cout<<s[len-1]; // last character of string 
+}
