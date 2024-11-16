ae8e3516490db1c6ec569fb3dfacd8e62161c5da
uni-coder-harsh <harsh06264@gmail.com>
2024-11-16 13:03:28 +0530

    Updated

--- DSA/Lec 1 Things to Know in CPP/1_patterns.cpp
+++ DSA/Lec 1 Things to Know in CPP/1_patterns.cpp
@@ -3,16 +3,14 @@ using namespace std;
 
 
 int main(){
-    int i ;
-    for ( i = 0; i <10; i++)
-    {
-        cout<<"harsh"<<" ";
-    }
-    do
-    {
-        cout<<"agdgadgdag";
-    }
-    while (i<10);
-    cout<<"harsh"<<" ";    
-    return 0 ;
+    // int i ;
+    // for ( i = 0; i <10; i++)
+    // {
+    //     cout<<"harsh"<<" ";
+    // }
+    // do
+    // {
+    //     cout<<"agdgadgdag";
+    // }
+    // while (i<10);
 }
